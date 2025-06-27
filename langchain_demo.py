5c24c62106b38657ad4a6824a569213e3910437a
georgeriya <riya.robins@gmail.com>
2025-06-27 13:21:36 +0530

    Add files via upload

---
+++ langchain_demo.py
@@ -0,0 +1,38 @@
+import re
+from html import unescape
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_nebius import ChatNebius
+
+load_dotenv()
+
+information = """
+Elon Reeve Musk FRS is a businessman. He is known for his leadership of Tesla, SpaceX, X (formerly Twitter), and the Department of Government Efficiency (DOGE). Musk has been considered the wealthiest person in the world since 2021; as of May 2025, Forbes estimates his net worth to be US$424.7 billion.
+
+Born to a wealthy family in Pretoria, South Africa, Musk emigrated in 1989 to Canada. He received bachelor's degrees from the University of Pennsylvania in 1997 before moving to California, United States, to pursue business ventures. In 1995, Musk co-founded the software company Zip2. Following its sale in 1999, he co-founded X.com, an online payment company that later merged to form PayPal, which was acquired by eBay in 2002. That year, Musk also became an American citizen.
+
+In 2002, Musk founded the space technology company SpaceX, becoming its CEO and chief engineer; the company has since led innovations in reusable rockets and commercial spaceflight. Musk joined the automaker Tesla as an early investor in 2004 and became its CEO and product architect in 2008; it has since become a leader in electric vehicles. In 2015, he co-founded OpenAI to advance artificial intelligence research but later left; growing discontent with the organization's direction and their leadership in the AI boom in the 2020s led him to establish xAI. In 2022, he acquired the social network Twitter, implementing significant changes and rebranding it as X in 2023. His other businesses include the neurotechnology company Neuralink, which he co-founded in 2016, and the tunneling company the Boring Company, which he founded in 2017. Musk was the largest donor in the 2024 U.S. presidential election, and is a supporter of global far-right figures, causes, and political parties. In early 2025, he served as senior advisor to United States president Donald Trump and as the de facto head of DOGE.
+"""
+if __name__ == "__main__":
+    print("hello langchain!!")
+
+    summary_template = """
+        given the information {information} about a person I want you to create:
+        1. a short summary
+        2. two interesting facts about them
+    """
+
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"], template=summary_template
+    )
+
+    llm = ChatNebius(temperature=0, model="Qwen/Qwen3-14B")
+
+    chain = summary_prompt_template | llm
+
+    res = chain.invoke(input={"information": information})
+
+    raw_content = res.content
+    cleaned = re.sub(r"<think>.*?</think>\s*", "", raw_content, flags=re.DOTALL)
+    cleaned = unescape(cleaned).strip()
+    print(cleaned)
